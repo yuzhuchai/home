35b13eb292b9198b273805632dd60d23853f85d4
yuzhu chai <yzhuchai@gmail.com>
2020-11-17 18:18:41 +0800

    adding existing code;

---
+++ app.js
@@ -0,0 +1,27 @@
+console.log('connected');
+
+
+let activeDiv 
+
+$('.button').click(function(){
+	// console.log('clicked')
+	$('.active').removeClass('active');
+	$(this).addClass('active');
+	activeDiv = $(this).attr('id');
+	console.log(activeDiv)
+	$('.activeContainer').removeClass('.activeContainer').addClass('inactiveContainer');
+	$(`#${activeDiv}Container`).addClass('activeContainer').removeClass('inactiveContainer');
+})
+
+
+
+$('.archiveIndexBlock').mousemove(function(){
+	// console.log(event.clientX, event.clientY)
+	$('.mouseText').css('left', event.clientX+20)
+	$('.mouseText').css('top', event.clientY+20)
+})
+
+
+$('.archiveIndexBlock').click(function(){
+	console.log('cliced')
+})
